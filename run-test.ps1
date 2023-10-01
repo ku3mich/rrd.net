72e36ba74a50af40dfcdc6ca0a787de0490488e7
Serhii Kuzmychov <ku3cmich@gmail.com>
2023-10-01 14:31:54 +0300

    fix test converage

--- run-test.ps1
+++ run-test.ps1
@@ -1,8 +1,7 @@
 Get-ChildItem -Recurse -Directory -Filter "*.Tests" |`
-	% { dotnet test --configuration Release `
+	% { dotnet test `
+		--configuration Release `
 		/p:CollectCoverage=true `
 		/p:CoverletOutputFormat=opencover `
-		/p:CoverletOutput=../.coverage/ `
-		/p:MergeWith=../.coverage/ `
 		$_.FullName 
 	}
